c1634da56c5b635c478fbc6b517f70d8ba21bfe6
aquamo@gmail.com <103864675+ishaqahmad41455@users.noreply.github.com>
2024-11-26 17:37:30 +0500

    fixes

--- modules/compute/public_ec2.tf
+++ modules/compute/public_ec2.tf
@@ -14,17 +14,4 @@ resource "aws_instance" "public-server" {
     # TeamDL      = local.TeamDL
     environment = "${var.environment}"
   }
-user_data = <<-EOF
-     #!/bin/bash
-     sudo apt update
-     sudo apt install nginx -y
-     sudo apt install git -y
-     sudo git clone https://github.com/saikiranpi/SecOps-game.git
-     sudo rm -rf /var/www/html/index.nginx-debian.html
-     sudo cp  SecOps-game/index.html /var/www/html/index.html
-     echo "<h1>${var.vpc_name}-public-Server-${count.index + 1}</h1>" >> /var/www/html/index.html
-     sudo systemctl start nginx
-     sudo systemctl enable nginx
- EOF
-
 }
